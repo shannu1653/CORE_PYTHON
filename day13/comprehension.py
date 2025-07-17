208707d24eb46d72eca809153066a94a8956d210
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-17 15:10:23 +0530

    comprehension

--- day13/comprehension.py
+++ day13/comprehension.py
@@ -4,3 +4,46 @@
 # 2.tuple comprehension 
 # 3.sets cimprehension 
 # 4.dictionat=ry comprehenison
+
+# op=[ x+2 for x in range(1,10) if x%2==0]
+# print(op)
+
+# op=[ x for x in range(1,10)]
+# print(op)
+
+# #create alist of suares of numbers from 1 to 20 using comprhension
+# op=[ x**2 for x in range(1,10)]
+# print(op)
+
+
+# #upper()--> it changes to lower case to upper case
+# list=['kiran','harish','harish','shannu']
+# op=[x.upper() for x in list]
+# print(op)
+
+# #lower()--> it changes  upper case lower case
+# list=['KIRAN', 'HARISH', 'HARISH', 'SHANNU']
+# op=[x.lower() for x in list]
+# print(op)
+
+# #suare of even numbers from 10 to 30
+# op=[ x**2 for x in range(10,31) if x%2==0]
+# print(op)
+
+
+# list=['kiran','harish','harish','shannu']
+# op=[x.upper() for x in list if len(x)&1==0]
+# print(op)
+
+def vowel(ip):
+    count=0
+    for x in ip:
+        if x in ['a','e','o','i','u']:
+            count+=1
+    if (count & 1==0):
+        return True
+    else:
+        return False
+list=['kiran','harish','harish','shannu']
+op=[x.upper() for x in list if vowel(x) ]
+print(op)
