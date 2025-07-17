16d92620bf2a8c21af7cf05f37f490287a33c6bb
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-17 14:33:07 +0530

    comprehension

---
+++ day13/comprehension.py
@@ -0,0 +1,6 @@
+
+#comprehension --> is a combination of  iteration,filtration(optional)  and expresion.
+#1.list comprehension 
+# 2.tuple comprehension 
+# 3.sets cimprehension 
+# 4.dictionat=ry comprehenison
