a276e880d12ac66c3e7113643bcfde6aeddd869f
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-17 16:20:22 +0530

    updated

--- day13/comprehension.py
+++ day13/comprehension.py
@@ -35,15 +35,70 @@
 # op=[x.upper() for x in list if len(x)&1==0]
 # print(op)
 
-def vowel(ip):
+# def vowel(ip):
+#     count=0
+#     for x in ip:
+#         if x in ['a','e','o','i','u']:
+#             count+=1
+#     if (count & 1==0):
+#         return True
+#     else:
+#         return False
+# list=['kiran','harish','harish','shannu']
+# op=[x.upper()  for x in list if vowel(x)]
+# print(op)
+
+# num=28
+# sum=0
+# for x in range(1,num):
+#     if num %x==0:
+#         sum+=x
+# if (sum==num):
+#     print("perfect number")
+# else: 
+#     print("not a perfect number")
+
+
+
+# def perfect(value):
+#     sum=0
+#     for x in range(1,value):
+#         if value %x==0:
+#          sum+=x
+#     if (sum==value):
+#      return True
+#     else: 
+#       return False
+
+# op=[x for x in range(1,50) if perfect(x)]
+# print(op)
+
+#create a list of armstrong numbers from 100 to 1000
+#do research how to generate tuple comprehension
+
+
+#armstrong number
+def armstrong(number):
+    number2=number
     count=0
-    for x in ip:
-        if x in ['a','e','o','i','u']:
-            count+=1
-    if (count & 1==0):
+    while (number>0):
+     num=number%10
+     count+=1
+     number=number//10
+    temp=number2
+    count2=0
+    while number2>0:
+     num=number2%10
+     count2+=num**count
+     number2=number2//10 
+    if count2==temp:
         return True
     else:
         return False
-list=['kiran','harish','harish','shannu']
-op=[x.upper() for x in list if vowel(x) ]
-print(op)
+op=[x for x in range(100,1000) if armstrong(x)]
+print(f"Armstrong numbers from range(100,1000) are {op}")
+
+
+
+
+
