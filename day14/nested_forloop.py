fbf3b3c1887266d564daac5367ee3a4466804fa0
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-18 15:31:26 +0530

    nested

---
+++ day14/nested_forloop.py
@@ -0,0 +1,16 @@
+#nested for loop
+#create list of perfectsquare,neon,sunny using comprehension
+#create list of revese string using comprehension
+
+# ip=['hello','shannu','naveen','akhil']
+def nested(num):
+    list=[]
+    for x in num:
+        rev=" "
+        for i in range(len(x)-1,-1,-1):
+            # print(x[i])
+            rev+=(x[i])
+        list.append(rev)
+    return list
+op=[x for x in nested(['shannu','naveen','sathish','tharun','akhil'])]
+print(op)
