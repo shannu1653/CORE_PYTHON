5b6a229ad148f21f0dd395497d5876906a32c785
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-19 15:27:27 +0530

    patterns

---
+++ day15/file.py
@@ -0,0 +1,34 @@
+
+#1
+count=""
+for i in range(5):
+    count +=str("*")
+    print(count)
+
+
+#2
+for i in range(0,6):
+    count=str(i)*(i)
+    print(count )
+#3
+name="shanmukha"
+name2=""
+for i in name:
+    name2+=i
+    print(name2)
+    
+#4
+name="abcde"
+count=0
+for i in name:
+    count+=1
+    print(i*(count))
+
+#5
+name="abcde"
+count=''
+for i in name:
+    count+=i
+    print(count)
+
+
