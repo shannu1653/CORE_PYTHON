99939f985a1e2148594499227363efbf40a48f2f
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-21 21:55:14 +0530

    problems

--- day14/problems.py
+++ day14/problems.py
@@ -1,11 +1,69 @@
-#check whether perfect suare or not
-num=16
+#1.check whether perfect square or not
+num=23
 is_perfect=False
 for x in range(1,num):
-    if (x**2==num):
+    if (x*x==num): #or if x**2==num
         is_perfect=True
         break
 if is_perfect:
     print('it is a perfect square')
 else : 
     print('it is not a perfect square')
+
+#2.sunny number-->num+1==square
+num=24
+is_sunny=False
+for x in range(1,num):
+    if (x*x==num+1):
+        is_sunny=True
+        break
+if is_sunny:
+    print("is is sunny number")
+else:
+    print("it is not sunny number")
+
+#3.neon number (9-->81--<8+1=9>)neon number
+
+number=9
+square=number**2
+sum=0
+while(number>0):
+    ld=square%10
+    sum=sum+ld
+    square=square//10
+if (sum==number):
+    print("the number is neon")
+else:
+    print("not a neon")
+
+
+#4.prime number
+def prime(num):
+    is_prime=True
+    if (num<=1):
+        print('please give a number grater than 1')
+    else:
+        for i in range(2,num):
+            if (num%i==0):
+                is_prime=False
+                break
+        if (is_prime):
+            return True
+        else:
+           return False
+
+op=[x for x in range(2,51) if prime(x)]
+print(op)
+
+op=tuple(x for x in range(2,51) if prime(x)) #tuple comprehwnsion using tuple generator
+print(op)
+
+op={x for x in range(2,51) if prime(x)}  #set comprenwnsion
+print(op)
+
+op=[x for x in range(2,51) if prime(x)]
+print(op)
+
+#mostly used list and set
+
+
