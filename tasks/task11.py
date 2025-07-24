46fbd56a63bf8edb37433a3b4db5350e8b8b526a
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-24 11:42:28 +0530

    task11

---
+++ tasks/task11.py
@@ -0,0 +1,41 @@
+# #Assignment - Math random module & keywords
+# #Generate 5 random floats between 0 and 1.
+import random
+# for i in range(5):
+#     print(random.random())
+
+# #2.Dice roll simulator using random.randint.
+# print("please roll a dice")
+# while True:
+#     number=random.randint(1,6)
+#     print("your number",number)
+
+#     again=input('try a again-(y/n) : ')
+#     if again.lower()!="y":
+#         print("good bye")
+#         break
+
+# #3.Convert 90 degrees to radians.
+# import math
+# pi=3.14
+# degrees=int(input("Enter degrees :  "))
+# radians=int(degrees*(pi/180))
+# print(f"{degrees} are in radains is {radians}")
+# print(math.radians(degrees))#using math function
+
+# #4.Factorial of a user-given number
+# import math
+# number=int(input("enter a factorail number : "))
+# print(math.factorial(number))
+# count=number
+# sum=1
+# for i in range(number,0,-1):
+#     sum =sum*i
+# print(sum)
+
+# #5. Shuffle a list of 10 integers.
+# a=[1,2,3,4,5,6,7,8,9,10]
+# random.shuffle(a)
+# print(a)
+
+
