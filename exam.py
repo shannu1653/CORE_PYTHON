b84b8c5ee2b8b21c84302262e572ae7c5c4b8b86
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-28 17:34:10 +0530

    exame

--- exam.py
+++ exam.py
@@ -20,29 +20,102 @@
 
 
 # a="abaabaabba"
-# list=''
+# lst=''
 # for i in a:
-#     if i not in list:
-#         list+=(i)
-# print((list))
+#     if i not in lst:
+#         lst+=(i)
+# print((lst))
 
 
 
-number = ("153 21 136 44 234").split()
+# number = ("153 21 136 44 234").split()
+# result = []
+# for i in number:
+#     number2 = list(i)
+#     length = len(number2)
+#     for j in range(length):
+#         min_index = j
+#         for x in range(j+1, length):
+#             if number2[x] < number2[min_index]:
+#                 min_index = x
+#         number2[j], number2[min_index] = number2[min_index], number2[j]
+#     sorted_number = ''.join(number2)
+#     result.append(sorted_number)
+# print(result)
 
-result = []  # empty list to collect sorted numbers
 
-for i in number:
-    number2 = list(i)  # convert string to list so we can swap
-    length = len(number2)
-    for j in range(length):
-        min_index = j
-        for x in range(j + 1, length):
-            if number2[x] < number2[min_index]:  # find the smallest digit
-                min_index = x
-        number2[j], number2[min_index] = number2[min_index], number2[j]
-    sorted_number = ''.join(number2)
-    result.append(sorted_number)  # add to result list
+# def is_prime(number):
+#     if number==2:
+#         return True
+#     else:
+#         for i in range(2,number):
+#             if number%i==0:
+#                 return False
+#             else :
+#                 return i
+# print(is_prime(int(input("Enter a number : "))))
 
-print(result)
 
+# def is_prime(number):
+#     if number==2:
+#         return True
+#     else:
+#         for i in range(2,number):
+#             if number%i==0:
+#                 return False
+#             else :
+#                 return True
+# c=0
+# for i in range(2,11):
+#     if  is_prime(i):
+#         c+=1
+# print(c)
+
+# n1='111'
+# n2='100'
+# sum=0
+# j=0
+# for i in range(len(n1)-1,-1,-1):
+#     sum +=int(n1[i])*(2**j)
+#     j+=1
+# k=0   
+# for i in range(len(n2)-1,-1,-1):
+#     sum +=int(n2[i])*(2**k)
+#     k+=1
+# a=str(bin(sum))
+# print(a)
+# print(a[2:])
+
+
+# name="sraaavan"
+# name2=''
+# for i in name:
+#     # name2+=i
+#     if i*i is name2 :
+#         continue
+#     else:
+#         name2+=i
+# print(name2)
+
+# s='sraaavan'
+# res=s[0]
+# c=1
+# for i in range(1,len(s)):
+#     if s[i]==s[i-1]:
+#         c+=1
+#         if c<3:
+#             res +=s[i]
+#     else:
+#             c=1
+#             res+=s[i]
+# print(res)
+
+
+amount=100
+total_choclets=amount
+w=amount
+while w>=3:
+    e=w//3
+    total_choclets+=e
+    w=e+e%3
+print(total_choclets)
