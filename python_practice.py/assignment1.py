dfb8101384ebd26a7446416afc280b627252f95b
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-06 22:20:41 +0530

    assignment

---
+++ python_practice.py/assignment1.py
@@ -0,0 +1,23 @@
+# for i in range(5):
+#     if i+1==3:
+#         break
+#     print("i:",i)
+#     print("Loop ended")
+
+# t=[[1,2],3]
+# t[0].append(4)
+# print(t)
+#append function adds the given element at the end of the list
+
+# s=""
+# for i in range(4):
+#     if i %2==0:
+#         s+='A'
+#     else:
+#         s+='B'
+# print(s)
+
+# word="Python3"
+# print(word[1:4]+word[-1]+word[:1])
+#slicing is used to obtain a part of a sequential value
+
