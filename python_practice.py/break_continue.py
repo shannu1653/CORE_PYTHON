5698e185d3ffdc6c1a646bd552bd9e7839b37a76
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-06 22:20:22 +0530

    break

---
+++ python_practice.py/break_continue.py
@@ -0,0 +1,30 @@
+# for x in range(1,10):
+#     print(x)
+#     if (x==5):
+#         break
+
+# for x in range(1,10):
+    
+#     if (x==5):
+#         continue
+#     print(x)
+
+# for x in range(1,10):
+    
+#     if (x==5):
+#         break
+#     print(x)
+
+# for x in range(1,10):
+#     print(x)
+#     if (x==5):
+#         continue
+#     x +=1
+#     print(x)
+
+stops=["kphb",'srnagar','ameerpet','paradegroun','paradice']
+for stop in stops:
+    # print(stop)
+    if (stop=="ameerpet"):
+        continue
+    print(stop)
