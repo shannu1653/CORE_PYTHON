c808dac5e29216418c1165a0dbbae03763bff756
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-06 22:20:57 +0530

    functions

---
+++ day9/function.py
@@ -0,0 +1,30 @@
+# def my_function():
+#     print("hello world")
+# my_function()
+
+# #arguments
+# def my_function(fname):#parameter
+#     print("hi",fname)
+# my_function('shannu')#arguments
+
+# #number of argumets
+# def my_function(name,age):
+#     print(f"hi {name} ,age is {age}")
+# my_function("shannu",22)
+
+# def my_function(name,age):
+#     print(f"hi {name} ,age is {age}")
+# my_function("shannu")
+
+
+# #*args(Arbitary arguments)
+# def my_function(*name):
+#     print(f"hi {name}")
+# my_function("shannu","naveen","mohan")
+
+
+#keyword arguments
+def my_function(name,age):
+    print("hi",name)
+    print("age",age)
+my_function(age=22,name="shannu")
