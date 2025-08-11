6321fe6fc9c7a6f6a4eaa29c7acdb7f410b53aac
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-11 22:49:16 +0530

    list function

--- day12/list-functions.py
+++ day12/list-functions.py
@@ -14,19 +14,59 @@ list2.insert(0,234343)#add but didnot remove original element
 print(list2)
 
 #4.replace()--> 
+
+
+
+
+
 #5.remove() -->remove particular element
 list2.remove(1)
 print(list2)
 
 #5.pop() -->remove last element in the list
+#or remove an element specified position
 list=[1,2,3,4,5,6,7]
 list.pop()
 print(list)
 
+##Note: The pop() method returns removed value.**************
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.pop(1)
+
+print(x)
+
+
 #6.count() -->to get the numbe of occurence of an element
-list=[1,2,33,22,2,2,2,23,3,3,44444444,3]
+list=[1,2,33,22,2,2,2,23,3,44444444,3]
 print(list.count(2))
 
 #7.clear():it removes all elements from the list
-list.clear()
-print(list)
+# list.clear()
+# print(list)
+
+#8.The copy() method returns a copy of the specified list.
+#syntax-list.copy()
+x=list.copy()
+print(x)
+
+#9.The index() method returns the position at the first occurrence of the specified value.
+# Syntax
+# list.index(elmnt, start, end)start	Optional. A number representing where to start the search
+                               #end	    Optional. A number representing where to end the search
+print(list.index(3))
+print(list.index(3,9))#Note: The index() method only returns the first occurrence of the value.
+
+#10.reverse() The reverse() method reverses the sorting order of the elements.
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+
+print(fruits)
+
+
+#11.sort()  Sort the list alphabetically:
+cars = ['Ford', 'BMWhffhf', 'Volvo']
+cars.sort(reverse=True)
+print(cars)
