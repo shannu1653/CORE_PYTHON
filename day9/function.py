a3cd9e178d8fdb50e786bc104cac610609d6be47
shannu1653 <pentashanmukha2002@gmail.com>
2026-01-01 22:48:26 +0530

    functions

--- day9/function.py
+++ day9/function.py
@@ -12,9 +12,9 @@
 #     print(f"hi {name} ,age is {age}")
 # my_function("shannu",22)
 
-# def my_function(name,age):
-#     print(f"hi {name} ,age is {age}")
-# my_function("shannu")
+def my_function(name,age=23):
+    print(f"hi {name} ,age is {age}")
+my_function("shannu")
 
 
 # #*args(Arbitary arguments)
@@ -24,7 +24,7 @@
 
 
 #keyword arguments
-def my_function(name,age):
-    print("hi",name)
-    print("age",age)
-my_function(age=22,name="shannu")
+# def my_function(name,age):
+#     print("hi",name)
+#     print("age",age)
+# my_function(age=22,name="shannu")
