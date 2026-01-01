e48c072a0c75775a48f314b65d8c1a340b44d34b
shannu1653 <pentashanmukha2002@gmail.com>
2026-01-01 22:47:15 +0530

    changes in variables

--- day 1/variable.py
+++ day 1/variable.py
@@ -5,3 +5,31 @@ print(x)
 print(y)
 print("just wait few days")
 print("hi")
+
+# '1.example for doctstring'
+# def square(n):
+#     '''Takes in a number n,return the square of n'''
+#     print(n**2)
+# square(5)
+# print(square.__doc__)
+
+'casting'
+print(str(3))
+print(int(3.0))
+print(float(3))
+print('shannu')
+print("shannu")
+# a,b,c=13
+# print(a)
+# print(c)
+
+#unpacking
+friuts=['shannu','naveen','tharun']
+x,y,z=friuts
+print(x)
+print(y)
+print(z)
+print(5,"5")
+
+name='shannu'
+print(isinstance(name, int)) #it checks data type gives boolean value
